1760acf70a6036eae847a000cec9e1458949523f
weiwei <chen_wei_wei@163.com>
2019-06-18 19:33:05 +0800

    'test'

--- src/main/scala/cn/bupt/tools/GetLatandLong.scala
+++ src/main/scala/cn/bupt/tools/GetLatandLong.scala
@@ -1,5 +1,41 @@
 package cn.bupt.tools
 
-object GetLatandLong {
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SQLContext
 
+object GetLatandLong {
+  def main(args: Array[String]): Unit = {
+    // 0 校验参数个数
+    if (args.length != 3) {
+      println(
+        """
+          |请正确传入参数
+          |InputPath：输入数据路径
+          |compressionCode <snappy, gzip, lzo>：数据压缩格式
+          |OutPutPath：输出数据路径
+        """.stripMargin)
+      sys.exit()
+    }
+    // 1 接受程序参数
+    val InputPath = args(0)
+    // 2 创建sparkconf->sparkContext
+    val sparkconf = new SparkConf()
+    sparkconf.set("spark.testing.memory", "2147480000")//后面的值大于512m即可
+    sparkconf.setAppName(s"${this.getClass.getSimpleName}").setMaster("local[*]")  //设置运行进程名
+    // RDD 序列化到磁盘 worker与worker之间的数据传输
+    sparkconf.set("spark.serializer","org.apache.spark.serializer.KryoSerializer")
+
+    val sc = new SparkContext(sparkconf)
+    val sQLContext = new SQLContext(sc)
+
+//    sQLContext.read.parquet(InputPath)
+//      //选择经纬度列数据
+//      .select("lat","long")
+//      //筛选中国境内的经纬度，排除空值
+//      .where("lat>3 and lat<54 and long>73 and long <136")
+//      //去重
+//      .distinct()
+//      .map()
+
+  }
 }
--- src/main/scala/cn/bupt/utils/BaiduGeoApi.java
+++ src/main/scala/cn/bupt/utils/BaiduGeoApi.java
@@ -1,4 +1,95 @@
 package cn.bupt.utils;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.lang.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class BaiduGeoApi {
+    public static String getBussine(String LatAndLong) throws Exception {
+        Map paramsMap = new LinkedHashMap<String, String>();
+        paramsMap.put("callback", "renderReverse");
+        paramsMap.put("location", "28.37,104.89");
+        paramsMap.put("output", "json");
+        paramsMap.put("pois", "1");
+        paramsMap.put("latest_admin", "1");
+        paramsMap.put("ak", "TmMZ5G4IViZbMsRnYibB73X0dBzSUmNf");
+
+        String businessString = null;
+
+        String paramsStr = toQueryString(paramsMap);
+        String wholeStr = new String("/geocoder/v2/?" + paramsStr + "IzdCBKL0whbHA7EkAiWPGwHYz5pYDMpp");
+        //System.out.println(wholeStr);
+
+        // 对上面wholeStr再作utf8编码
+        String tempStr = URLEncoder.encode(wholeStr, "UTF-8");
+        // 调用下面的MD5方法得到最后的sn签名7de5a22212ffaa9e326444c75a58f9a0
+        //System.out.println(MD5(tempStr));
+
+        //相当于浏览器
+        HttpClient httpClient = new HttpClient();
+        GetMethod getMethod = new GetMethod("http://api.map.baidu.com/geocoder/v2/?"+paramsStr+"&sn="+MD5(tempStr));
+        //返回访问浏览器状态码
+        int code = httpClient.executeMethod(getMethod);
+        if(code == 200){
+            String responseBody = getMethod.getResponseBodyAsString();
+            //关闭getMethod
+            getMethod.releaseConnection();
+            String replaced = responseBody.replace("renderReverse&&renderReverse(", "");
+            String substring = replaced.substring(0, replaced.lastIndexOf(")"));
+            //System.out.println(substring);
+            //解析json字符串 -- fastJson
+            JSONObject jsonObject = JSON.parseObject(substring);
+            JSONObject resultJson = jsonObject.getJSONObject("result");
+            businessString = resultJson.getString("business");
+            //System.out.println(businessString);
+            if(StringUtils.isEmpty(businessString)){
+                JSONArray poisArray = resultJson.getJSONArray("pois");
+                if(poisArray.size()>0){
+                    businessString = poisArray.getJSONObject(0).getString("tag");
+                }
+            }
+            System.out.println(businessString);
+        }
+        return businessString;
+    }
+
+    // 对Map内所有value作utf8编码，拼接返回结果
+    private static String toQueryString(Map<?, ?> data)
+            throws UnsupportedEncodingException {
+        StringBuffer queryString = new StringBuffer();
+        for (Entry<?, ?> pair : data.entrySet()) {
+            queryString.append(pair.getKey() + "=");
+            queryString.append(URLEncoder.encode((String) pair.getValue(),
+                    "UTF-8") + "&");
+        }
+        if (queryString.length() > 0) {
+            queryString.deleteCharAt(queryString.length() - 1);
+        }
+        return queryString.toString();
+    }
+
+    // 来自stackoverflow的MD5计算方法，调用了MessageDigest库函数，并把byte数组结果转换成16进制
+    private static String MD5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest
+                    .getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100)
+                        .substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+        }
+        return null;
+    }
 }
